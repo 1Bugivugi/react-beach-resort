df644025eba3011706b7af862253a83f5f65dd7f
Gleb Rezoun <gleb.rezun@gmail.com>
2019-12-06 03:13:59 +0300

    Init commit

---
+++ src/components/Wrapper.js
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react';
+
+/**
+ * ComponentName
+ */
+export class ComponentName extends Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>MY COMPONENT</div>
+    );
+  }
+}
+
+export default ComponentName;
